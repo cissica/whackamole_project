b8fb4830568cfe5b439fd32105415155517a5b82
cissica <cecemartinez@hotmail.com>
2021-05-27 15:08:49 -0700

    user methods

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -5,5 +5,17 @@ class User{
         this.id = id
     }
 
-    
+    welcomeUser(){
+        let header = document.querySelector('#usersname')
+        const userName = this.name
+        header.innerHTML +=  ` Welcome, ${userName}.`
+    }
+
+    fromForm(){
+        // const userForm = document.querySelector('.register')
+        let name = document.getElementById("name").value 
+        this.name = name 
+        
+    }
+
 }
