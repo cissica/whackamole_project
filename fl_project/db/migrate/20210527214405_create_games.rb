85ae06cb8ff9998a2f2cf7e1c1291b95e777dbea
cissica <cecemartinez@hotmail.com>
2021-05-27 14:58:32 -0700

    tables created

--- fl_project/db/migrate/20210527214405_create_games.rb
+++ fl_project/db/migrate/20210527214405_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
       t.string :score
-
+      t.belongs_to :user, foreign_key: true
       t.timestamps
     end
   end
